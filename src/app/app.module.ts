09c70d4c8335a3a8c6a6610954ec18da5083a7fb
BehzodRj <behzodfarishta@gmail.com>
2021-09-16 11:21:50 +0300

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthPageComponent } from './auth-page/auth-page.component';
-import { CheckboxModule, WavesModule, ButtonsModule, InputsModule, IconsModule, CardsModule } from 'angular-bootstrap-md';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,15 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    FormsModule,
-    ReactiveFormsModule,
-    CheckboxModule,
-    WavesModule,
-    ButtonsModule,
-    InputsModule, 
-    IconsModule, 
-    CardsModule,
-    
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
